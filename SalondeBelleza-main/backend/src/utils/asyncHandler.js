48283c5cec463538dd662d7e4f81105828f26ce1
joseluis723 <jluisalvarez1@outlook.com>
2026-09-03 22:26:25 -0400

    Delete SalondeBelleza-main directory

--- SalondeBelleza-main/backend/src/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// Evita repetir try/catch en cada ruta async
-module.exports = function asyncHandler(fn) {
-  return function (req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-};
